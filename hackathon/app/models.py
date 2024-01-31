a1302f269d6b0e6995b4429623a2c01e924b9b8d
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2024-01-31 16:09:28 +0400

    new

--- hackathon/app/models.py
+++ hackathon/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=200)
+    organisor = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    date = models.DateField()
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- hackathon/app/views.py
+++ hackathon/app/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, HttpResponse
+from .models import Campaign
+
 
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
 
 def campaigns(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        organisor = request.POST.get('organisor')
+        location = request.POST.get('location')
+        date = request.POST.get('date')
+        description = request.POST.get('description')
+        
+        Campaign.objects.create(name=name, organisor=organisor, location=location, date=date, description=description)
     return render(request, 'app/campaigns.html')
 
 def contact(request):
