20907e621f9a861a4b1b076c4dab21da32c4f325
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2024-06-05 21:28:10 +0400

    new

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [BASE_DIR / 'static']
 
 MEDIA_ROOT = BASE_DIR / "media"
