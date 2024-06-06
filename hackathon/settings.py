c06becade0ac0f4969443c455a68b6558ebb2e59
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2024-06-06 14:06:20 +0400

    new

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -22,11 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+# SECRET_KEY = 'django-insecure-q0b(0uhuwrij#_(w_uimxifyrtm)5yf(*hxw_qm^*5i7dntr-d'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
