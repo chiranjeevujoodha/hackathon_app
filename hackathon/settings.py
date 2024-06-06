52deb411bdac3757157d4ab1de74b1cc5f2327de
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2024-06-06 10:36:47 +0400

    new

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -141,9 +141,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIR = [BASE_DIR / 'static']
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
