7abad97ca03110b8f3e79da4f52978ad63de3851
chiranjeevujoodha <chiranjeev.ujoodha@rogershospitality.com>
2024-06-07 20:00:55 +0400

    new

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
 
